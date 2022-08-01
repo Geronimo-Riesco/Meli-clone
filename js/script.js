e822032382a379ee73ccc28cfd0a901feacd6d83
Gerónimo Ezequiel Riesco <83089714+Geronimo-Riesco@users.noreply.github.com>
2022-08-01 13:47:44 -0300

    JavaScript

---
+++ js/script.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal("#banner-one");
+ScrollReveal().reveal("#best-work", { delay: 500 });
+ScrollReveal().reveal("#banner-best-work", { delay: 900 });
+ScrollReveal().reveal("#banner-two", { delay: 900 });
+ScrollReveal().reveal("#banner-three", { delay: 900 });
+ScrollReveal().reveal("#banner-four", { delay: 900 });
+ScrollReveal().reveal("#banner-five", { delay: 900 });
+ScrollReveal().reveal("#analitys-meli", { delay: 900 });
+ScrollReveal().reveal("#banner-six", { delay: 900 });
